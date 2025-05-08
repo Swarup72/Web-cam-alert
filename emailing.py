bc926117cebe4513f63da091dc0c9c5583771cfa
swaruppal <swaruppal1511@gmail.com>
2025-05-08 16:59:58 +0530

    the project is all done and ready to go

--- emailing.py
+++ emailing.py
@@ -1,2 +1,26 @@
-def send_email():
-    print("Sending email.")
+import smtplib
+import imghdr
+from email.message import EmailMessage
+
+PASSWORD="ynachlnxkglvoeby"
+SENDER = "swarup111505@gmail.com"
+RECEIVER = "swarup111505@gmail.com"
+
+def send_email(image_path):
+    print("email start")
+    email_message = EmailMessage()
+    email_message["Subject"] = "New customer showed up!!"
+    email_message.set_content("Hey, there is new customer..")
+
+    with open(image_path,"rb") as file:
+        content = file.read()
+    email_message.add_attachment(content,maintype = "image",subtype= imghdr.what(None,content))
+
+    gmail =  smtplib.SMTP("smtp.gmail.com", 587)
+    gmail.ehlo()
+    gmail.starttls()
+    gmail.login(SENDER,PASSWORD)
+    gmail.sendmail(SENDER,RECEIVER,email_message.as_string())
+    gmail.quit()
+    print("email ended")
+
